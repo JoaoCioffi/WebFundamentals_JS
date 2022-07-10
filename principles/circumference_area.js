935b7a7862264bafcc35d341c3b15722514db0dc
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-10 19:11:56 -0300

    Create circumference_area.js

---
+++ principles/circumference_area.js
@@ -0,0 +1,17 @@
+/*
+*
+* Circumference Area
+*      Calculator
+*
+*/
+
+{
+    // input
+    var radius = 5.;
+    var area = Math.PI * (radius**2);
+
+    // prints the value
+    {
+        console.log('Area is: ', area, 'm²')
+    }
+}
