9dc5cc0dd51de9f0f024c3dedb00d813b262fd93
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-10 18:18:10 -0300

    Update datatypes.js

--- principles/datatypes.js
+++ principles/datatypes.js
@@ -1,1 +1,26 @@
+/*
+*
+* Data Types
+* in JavaScript
+*
+*/
 
+{
+    // defining variables
+    let x = 1.225;
+    let y = 'hello world';
+    let z = 0;
+    let xx = false;
+    let yy = NaN;
+    let zz = null;
+    
+    // reading dtypes
+    {
+        console.log(typeof x);
+        console.log(typeof y);
+        console.log(typeof z);
+        console.log(typeof xx);
+        console.log(typeof yy);
+        console.log(typeof zz);
+    }
+}
