e17a22c698c6723ea3c9b6bdd2606609ffcc0716
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-11 17:37:55 -0300

    Update stringTypes.js

--- principles/stringTypes.js
+++ principles/stringTypes.js
@@ -7,7 +7,6 @@ console.log(user.indexOf('d'))
 console.log(user.indexOf('3'))
 console.log(user.indexOf('r'))
 
-
 // Gets the element using positions
 console.log(user.charAt(0))
 console.log(user.charAt(1))
@@ -21,3 +20,14 @@ console.log(user.charCodeAt(1))
 console.log(user.charCodeAt(2))
 console.log(user.charCodeAt(3))
 console.log(user.charCodeAt(4))
+
+// Returns a substring from a specific position (can be limited)
+console.log(user.substring(0))
+console.log(user.substring(1))
+console.log(user.substring(2))
+console.log(user.substring(3))
+console.log(user.substring(4))
+console.log(user.substring(0,4))
+
+// Concatenate
+console.log(('username: ').concat('_').concat(user).concat('_'))
