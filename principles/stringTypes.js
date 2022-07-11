fb9af2d2beffe6683bfc197228af8bee3be579b3
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-11 17:31:27 -0300

    Create stringTypes.js

---
+++ principles/stringTypes.js
@@ -0,0 +1,23 @@
+let user = 'Cod3r'
+
+// Gets the position using element
+console.log(user.indexOf('C'))
+console.log(user.indexOf('o'))
+console.log(user.indexOf('d'))
+console.log(user.indexOf('3'))
+console.log(user.indexOf('r'))
+
+
+// Gets the element using positions
+console.log(user.charAt(0))
+console.log(user.charAt(1))
+console.log(user.charAt(2))
+console.log(user.charAt(3))
+console.log(user.charAt(4))
+
+// unicode table value for these elements
+console.log(user.charCodeAt(0))
+console.log(user.charCodeAt(1))
+console.log(user.charCodeAt(2))
+console.log(user.charCodeAt(3))
+console.log(user.charCodeAt(4))
