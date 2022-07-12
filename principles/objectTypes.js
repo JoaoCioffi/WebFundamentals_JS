51334df81e274d5903b17e7e44a4325a5e6a79d7
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-12 10:55:35 -0300

    Update objectTypes.js

--- principles/objectTypes.js
+++ principles/objectTypes.js
@@ -10,7 +10,7 @@ sale.customer_id = 10056;
 sale.product_id = product_01.id;
 sale.qty = 15;
 sale.discount = 0.05;
-sale.total = `U$${(sale.qty*product_01.price)-(sale.discount*(sale.qty*product_01.price))}`;
+sale.total = `U$${((sale.qty*product_01.price)-(sale.discount*(sale.qty*product_01.price))).toFixed(2)}`;
 
-console.log(product_01);
-console.log(sale);
+console.log(product_01)
+console.log(sale)
