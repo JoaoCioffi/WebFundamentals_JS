c26f549532bc3017a8ffe0a7132a89bca5c2a2da
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-11 21:43:25 -0300

    Create arrayTypes.js

---
+++ principles/arrayTypes.js
@@ -0,0 +1,10 @@
+let values = [1.00,2.25,0.23,
+              3.48,8.12,7.55,
+             -2.25,0.55,9.81];
+
+console.log(typeof values)
+console.log(values[2],values[7]);
+console.log(values.length)
+
+let values_2 = [[1,2,3],true,Infinity,NaN,'teste',{x:1.23}]
+console.log(values_2)
