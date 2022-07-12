7e383841b3b548b9f819a17b439a738783077723
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-12 14:45:27 -0300

    Update loopFor.js

--- principles/loopFor.js
+++ principles/loopFor.js
@@ -4,8 +4,21 @@ for (var i=0; i<16; i++){
 }
 console.log('i = ',i)
 
-// with let
-for (let x=0; i<16; i++){
-    console.log(`sqrt(${x}) = ${(x**0.5).toFixed(3)}`)
+const funcs = []
+for (var i=0; i<10; i++){
+    funcs.push(function(){
+        console.log(i)
+    })
 }
-console.log('x = ',x)
+funcs[2]()
+funcs[8]()
+
+// with let
+const funcs2 = []
+for (let i=0; i<10; i++){
+    funcs2.push(function(){
+        console.log(i)
+    })
+}
+funcs2[2]()
+funcs2[8]()
