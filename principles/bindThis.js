9406ce8607f6b4f8a37a54b28508df49ce8a22b5
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-14 14:07:19 -0300

    Update bindThis.js

--- principles/bindThis.js
+++ principles/bindThis.js
@@ -9,3 +9,16 @@ bot.say()
 
 const say = bot.say.bind(bot)
 say()
+
+
+function person() {
+    this.age = 0
+    
+    const self = this
+    setInterval(function() {
+        self.age++
+        console.log(self.age)
+    }/*.bind(this)*/, 1000)
+}
+
+new person
