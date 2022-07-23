89658dcf78b25bff586a5bed1ecd4f8c07900742
JoaoCioffi <joaorcioffi@gmail.com>
2022-07-23 18:08:05 -0300

    writing files

---
+++ node/writingFiles/writingFiles.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const product = {
+    name: 'Jasper Knox Mug',
+    price: 12.89,
+    discount: 0.05,
+    category: 'utensils'
+}
+
+
+fs.writeFile(__dirname + '/generatedFile.json', JSON.stringify(product), err => {
+    console.log(err || 'saved file successfully!')
+})
