4ab608f872124c17e61967a18a1cffc02d3042f2
JoaoCioffi <joaorcioffi@gmail.com>
2022-07-23 21:10:58 -0300

    updated project -- stable version

--- node/modules/project/src/server.js
+++ node/modules/project/src/server.js
@@ -2,14 +2,34 @@ const port = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const database = require('./database.js')
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/products', (request,response,next) => {
-    console.log('Moddleware 1...')
+    console.log('Middleware 1...')
     next()
 })
 
 app.get('/products', (request,response,next) => {
+    response.send(database.getProducts())
+})
+
+app.get('/products/:id',(request,response,next) => {
+    response.send(database.getProduct(request.params.id))
+})
+
+app.get('/products', (request,response,next) => {
     response.send({name:'noetic-sierra',status:'active'}) // converts to JSON
+})
+
+app.post('/products',(request,response,next) => {
+    const product = database.saveProduct({
+        name: request.body.name,
+        price: request.body.price
+    })
+    response.send(product) // JSON
 })
 
 app.listen(port, () => {
